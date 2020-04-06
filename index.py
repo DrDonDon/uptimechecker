c5d1918b3bcb6a8b17e4528ec08db845811d95af
DrDonDon <53420422+DrDonDon@users.noreply.github.com>
2020-04-06 15:38:49 +1000

    Update to new SDK

--- index.py
+++ index.py
@@ -1,19 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 15:25:08 2020
+
+@author: Isaac
+"""
+
 from dotenv import load_dotenv
 load_dotenv()
  
 import requests
 import time
+from datetime import datetime
 import os
-import amphora_client
-from amphora_client.rest import ApiException
-from amphora_client.configuration import Configuration
+from amphora.client import AmphoraDataRepositoryClient, Credentials
+import amphora_api_client as a10a
+from amphora_api_client.rest import ApiException
+from amphora_api_client.configuration import Configuration
 
 ad_url = "https://beta.amphoradata.com/healthz"
 id = "a6f2a8b4-0c72-402e-b820-90aedecea14f"
 
 def get_isup():
     r = requests.get(ad_url)
-    return (r.status_code == 200 and len(r.content) == 0)
+    return (r.status_code == 200 and len(r.content) == 7)
 
 isup_status = get_isup()
 
@@ -22,28 +31,30 @@ if (isup_status):
 else:
     print("It ain't up")
 
-configuration = Configuration()
-auth_api = amphora_client.AuthenticationApi(amphora_client.ApiClient(configuration))
-token_request = amphora_client.TokenRequest(username=os.getenv('username'), password=os.getenv('password') )
+credentials = Credentials(username==os.getenv('username'), password=os.getenv('password'))
+client = AmphoraDataRepositoryClient(credentials)
 
-try:
+
+#try:
     # Gets a token
-    res = auth_api.authentication_request_token(token_request = token_request)
-    configuration.api_key["Authorization"] = "Bearer " + res
+    #res = auth_api.authentication_request_token(token_request = token_request)
+    #configuration.api_key["Authorization"] = "Bearer " + res
     # create an instance of the Users API, now with Bearer token
-    users_api = amphora_client.UsersApi(amphora_client.ApiClient(configuration))
-    me = users_api.users_read_self()
-    print(me)
-except ApiException as e:
-    print("Exception when calling AuthenticationAPI: %s\n" % e)
+    #users_api = amphora_client.UsersApi(amphora_client.ApiClient(configuration))
+    #me = users_api.users_read_self()
+    #print(me)
+#except ApiException as e:
+#    print("Exception when calling AuthenticationAPI: %s\n" % e)
 
-amphora_api = amphora_client.AmphoraeApi(amphora_client.ApiClient(configuration))
+amphora_api = a10a.AmphoraeApi(client.apiClient)
+amphora = client.get_amphora(id ) 
 
+Signal = []
 try:
     if (isup_status):
-        s = {'isup': 1}
+        Signal.append(dict(t = datetime.utcnow(),isup = 1))
     else:
-        s = {'isup': 0}
-    amphora_api.amphorae_signals_upload_signal(id, request_body=s)
+        Signal.append(dict(t = datetime.utcnow(),isup = 0))
+    amphora.push_signals_dict_array(Signal)  
 except ApiException as e:
     print("Exception when calling AmphoraeApi: %s\n" % e)
